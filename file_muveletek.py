62fc49b0c805f4facc0b8626d9bc4472cd748a0b
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2023-04-23 17:16:27 +0200

    2. alkalom

--- file_muveletek.py
+++ file_muveletek.py
@@ -25,9 +25,9 @@ függetlenül attól, hogy te rendelezel e róla
 def beolvas():
 
     temp = "Ricsi"
-    with open("kerites.txt", "r") as f:
+    with open("1. alkalom/kerites.txt", "r") as f:
     # read() a file teljes tartalmát kiolvassa és egy stringként adja vissza
-    #data = f.read().split('\n')
+        #data = f.read().split('\n')
         data = f.readlines()
     return data
 
@@ -59,10 +59,163 @@ def feladat_3():
         print("A páratlan oldalon adták el az utolsó telket")
         print(f"Az utolsó telek házszáma:{paratlan_db} ")
 
+"""
+4. Írjon a képernyőre egy házszámot a páratlan oldalról, amely melletti telken ugyanolyan
+színű a kerítés! (A hiányzó és a festetlen kerítésnek nincs színe.) Feltételezheti, hogy van
+ilyen telek, a több ilyen közül elég az egyik ház számát megjeleníteni. 
+"""
+
+# packing és unpacking
+
+a, b = 10, 20
+
+# unpacking művelet
+a, b = [10, 20]
+a, b = (10, 20)
+# *b -> packing művelet
+a, *b = [10, 20, 30]
+
+# print(a, b)
+
+
+
+def negyedik_feladat():
+    temp = [] 
+    for item in data: 
+        sor = item.split(' ')
+
+        if int(sor[0]) == 1:
+            temp.append(sor)
+
+    # mivel a temp-ben csak a páratlanok vannak
+    # a lista indexei 0-tól indulnak
+    # ha megvan az index értéke, akkor neküókn úgy kell majd számolnunk
+    # figyelembe kell venni, hogy a pátarlan házszámok 1-el kezdődnek
+
+    # enumerate függvény: enumerate(['Ricsi', 'Karcsi', 'Gabi']) -> [(0, 'Ricsi), (1, 'Karcsi'), (2, 'Gabi')]
+    for idx, item in enumerate(temp):
+        # print(item)
+        # a színből replacelem a sortörést: a \n a sortörés karakter
+        color = item[-1].replace('\n', '')
+
+        if color in (':', '#'):
+            continue
+
+        if temp[idx+1][-1].replace('\n', '') == color:
+            haz_szam = idx * 2 + 1
+            print(f"A szomszédossal egyezik a kerítés színe: {haz_szam} ")
+            break
+        else:
+            continue
+
+def feladat_5():
+    import string
+    # hazszam = int(input(" addj meg egy házszámot \n"))
+
+    hazszam = 83
+    
+    """
+    a. Írja ki a házszámhoz tartozó kerítés színét, ha már elkészült és befestették,
+    egyébként az állapotát a „#” vagy „:” karakter jelöli!
+    """
+    temp = []
+    haz_idx = 0
+    for item in data: 
+        sor = item.split(' ')
+
+        if hazszam % 2 == 0:
+            if int(sor[0]) == 0:
+                temp.append(sor)
+                haz_idx = int(hazszam/2) -1
+        else:
+            if int(sor[0]) == 1:
+                temp.append(sor)
+                haz_idx = int((hazszam -1) / 2)
+
+    color = temp[haz_idx][-1].replace('\n', '')
+    print(temp[haz_idx])
+    print(f"A kerítés színe / állapota: {color}")
+
+    # ha a legelső számot adtuk meg, akkor csak 1 szomszédja van: utána lévő
+    # ha az utolsó számot adtuk meg, akkor is csak 1 szomszédja: előtte lévő
+
+    if haz_idx == 0:
+        colors = []
+
+        next_color = temp[haz_idx + 1][-1].replace('\n', '')
+        colors.append(color)
+        colors.append(next_color)
+
+    elif haz_idx == len(temp)-1:
+        colors = []
+
+        prev_color = temp[haz_idx -1][-1].replace('\n', '')
+        colors.append(color)
+        colors.append(prev_color)
+
+    else:
+        colors = []
+
+        colors.append(color)
+        prev_color = temp[haz_idx -1][-1].replace('\n', '')
+        next_color = temp[haz_idx + 1][-1].replace('\n', '')
+
+        colors.append(prev_color)
+        colors.append(next_color)
+
+    print(colors)
+    """
+    A házszámhoz tartozó kerítést szeretné tulajdonosa be- vagy átfesteni. Olyan
+színt akar választani, amely különbözik a mellette lévő szomszéd(ok)tól és a
+jelenlegi színtől is. Adjon meg egy lehetséges színt! A színt a teljes palettából
+(A–Z) szabadon választhatja meg.
+    """
+    alphabet = string.ascii_uppercase
+
+    for item in alphabet:
+        if item not in colors:
+            print(f"Egy lehetséges festési szín: {item}")
+            break
+
+def feladat_6():
+    temp = []
+
+    for item in data: 
+        sor = item.split(' ')
+
+        if int(sor[0]) == 1:
+            temp.append(sor)
+
+    draw_color = ""
+    house_numbers = ""
+    for idx, item in enumerate(temp):
+        color = item[-1].replace('\n', '')
+        
+        if idx == 0:
+            val = 1
+        else:
+            val += 2
+
+        draw_color += color*int(item[1])
+
+        house_numbers += str(val) + " " *(int(item[1])-len(str(val)))
+
+
+        # house_numbers += draw_color
+
+    with open("utackep.txt", "w") as f:
+        f.write(draw_color)
+        f.write('\n')
+        f.write(house_numbers)
+
+
+
 data = beolvas()
 feladat_1()
 feladat_3()
-
+negyedik_feladat()
+feladat_5()
+feladat_6()
 # if __name__ == '__main__':
 #     data = beolvas()
 
